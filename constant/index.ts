4c7ea0baacced4ef00dbe4522d172e14b04f7fcd
ShubhamSingh-28 <ss9971768704@gmail.com>
2025-11-11 11:09:09 +0530

    url

--- constant/index.ts
+++ constant/index.ts
@@ -1,6 +1,6 @@
 import { Platform } from "react-native";
 
-export const API_URL =Platform.OS=="android"?'http://192.168.128.254:3000':'http://localhost:3000';
+export const API_URL =Platform.OS=="android"?'https://bubbly-backend.onrender.com':'https://bubbly-backend.onrender.com';
 
 export const CLOUDINARY_CLOUD_NAME="shubham28";
 export const CLOUDINARY_UPLOAD_PRESET="images";
